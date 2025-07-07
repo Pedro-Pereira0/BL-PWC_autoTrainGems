c48a3df84985fc857e029caa55c1d201a3afd3fd
1201671-Pedro <1201671@isep.ipp.pt>
2025-07-07 22:35:37 +0100

    a

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from util.Util import Util
 import threading
 
 listener_thread = threading.Thread(target=Util.exitIfKeyPressed)
-listener_thread.daemon = True  # Thread exits when main program ends
+listener_thread.daemon = True 
 listener_thread.start()
 
 View().startTraining()
-
