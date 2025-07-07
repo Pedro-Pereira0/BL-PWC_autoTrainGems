41a31c002de7eee826564063eb5dff024c174cd1
1201671-Pedro <1201671@isep.ipp.pt>
2025-07-07 22:23:18 +0100

    changed Util method to static

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from view.View import View
 from util.Util import Util
 import threading
 
-util =  Util()
-
-listener_thread = threading.Thread(target=util.exitIfKeyPressed)
+listener_thread = threading.Thread(target=Util.exitIfKeyPressed)
 listener_thread.daemon = True  # Thread exits when main program ends
 listener_thread.start()
 
--- util/util.py
+++ util/util.py
@@ -4,8 +4,8 @@ import os
 class Util:
     def __init__(self):
         pass
-
-    def exitIfKeyPressed(self, combo=['ctrl', 'q']):
+    @staticmethod
+    def exitIfKeyPressed(combo=['ctrl', 'q']):
         print("Key listener thread started.")
         while(True):
             if keyboard.is_pressed(combo):  # if combination of 'ctrl and q' is pressed 
