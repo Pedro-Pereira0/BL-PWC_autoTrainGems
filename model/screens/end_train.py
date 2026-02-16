8e4f384b141f26ee1f62cbd53f977858b921a0f2
1201671-Pedro <1201671@isep.ipp.pt>
2026-02-16 11:56:55 +0000

    Change in end train screen, now it clicks until it finds the training mission progress

--- model/screens/end_train.py
+++ model/screens/end_train.py
@@ -4,6 +4,7 @@ class EndTrain(Screen):
     def __init__(self,options):
         super().__init__(options)
         self.finishButton:str = options.imagePath + "control/finishButton.jpg"
+        self.missionProgress:str = options.imagePath + "control/TMP.jpg"
         self.cancelButton:str = options.imagePath + "control/cancelButton.jpg"
     def run(self):
         print("This is the End Training Menu")
@@ -12,14 +13,23 @@ class EndTrain(Screen):
         buttonX, buttonY = self.findImage(self.finishButton)
         self.clickOnImage(buttonX,buttonY)
 
-        time.sleep(3)
+        while True:
+            buttonX1, buttonY2 = self.findImage(self.missionProgress)
+            if buttonX1 != None and buttonY2 != None:
+                self.clickOnImage(buttonX, buttonY)
+                break
+            else:
+                self.clickOnImage(buttonX, buttonY)
+
+        time.sleep(5)
 
         self.clickOnImage(buttonX, buttonY)
 
         time.sleep(5)
 
         self.clickOnImage(buttonX, buttonY)
-        self.clickOnImage(buttonX, buttonY)
+
+        time.sleep(5)
 
         buttonX, buttonY = self.findImage(self.cancelButton)
         self.clickOnImage(buttonX, buttonY)
